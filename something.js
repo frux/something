777009fd6998e10bc8b12af027220e254c1495cf
Vladimir Kudinov <frux@yandex-team.ru>
2015-12-04 18:30:18 +0500

    NPM

--- something.js
+++ something.js
@@ -1,6 +1,12 @@
+/**
+ * Something.js
+ * Adds something to your global
+ * @author frux
+ * @url https://github.com/frux/something
+ */
+
 (function(){
 	var _global = ((typeof window === 'object') ? window : global);
-
 	Object.defineProperty(_global, 'something', {
 		get: function(){
 			var keys = Object.keys(_global);
